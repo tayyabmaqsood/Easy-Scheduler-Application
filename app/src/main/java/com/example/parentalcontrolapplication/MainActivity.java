d9e76a98868d5f7a4838d1eb9bc7464b0fdecc69
tayyabmaqsood <tayyabmaqsood48@gmail.com>
2022-01-23 17:12:27 +0500

    Registration form completed

--- app/src/main/java/com/example/parentalcontrolapplication/MainActivity.java
+++ app/src/main/java/com/example/parentalcontrolapplication/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.parentalcontrolapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+
+
+    public void createNewAccount(View view) {
+        startActivity(new Intent(this,SignUpActivity.class));
     }
 }
---
+++ app/src/main/java/com/example/parentalcontrolapplication/SignUpActivity.java
@@ -0,0 +1,42 @@
+package com.example.parentalcontrolapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+public class SignUpActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        Spinner spinner = (Spinner) findViewById(R.id.userTypeSpinner);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,R.array.userTypeArray, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+        spinner.setOnItemSelectedListener(this);
+    }
+
+    public void loginAccount(View view) {
+        startActivity(new Intent(this, MainActivity.class));
+    }
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+       TextView userTypeTextView = findViewById(R.id.userTypeTextview);
+        if(!adapterView.getItemAtPosition(i).toString().equals("Select"))
+            userTypeTextView.setText(adapterView.getItemAtPosition(i).toString());
+        else
+        userTypeTextView.setText("User Type");
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+}
