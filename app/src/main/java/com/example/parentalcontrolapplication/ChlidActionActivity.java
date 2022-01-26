5c1ec378462b52581cd2b54e7f45b13e2d5277d8
tayyabmaqsood <tayyabmaqsood48@gmail.com>
2022-01-27 00:28:53 +0500

    child action activity completed

---
+++ app/src/main/java/com/example/parentalcontrolapplication/ChlidActionActivity.java
@@ -0,0 +1,14 @@
+package com.example.parentalcontrolapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ChlidActionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chlid_action);
+    }
+}
